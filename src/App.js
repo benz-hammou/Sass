4db4a84c40b1fd7419833a895aa25b76390a0799
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-04-18 10:18:10 +0200

    r

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import './stylesheets/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          <h1>    I am green
+          </h1>
+        </a>
+
+        <h1>    I am out
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
